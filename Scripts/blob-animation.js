9c04ed2d44dd87481aaf8f3003184384a99b044e
Aidar <sharipovaydar200515@gmail.com>
2023-09-19 10:41:23 +0600

    Закомментировал анимацию

--- Scripts/blob-animation.js
+++ Scripts/blob-animation.js
@@ -1,31 +1,40 @@
+// Референс на тот кусок цвета
 const blob = document.getElementById("blob")
 
+// Координаты мыши
 var mouseX = 0
 var mouseY = 0
 
+// Показывает позицию скролла по Y
+var scrollPositionY = 0
 
+
+// Анимация елемента на движения курсора, с учетом позиции скролла
 document.body.onpointermove = event => {
     const { clientX, clientY} = event
-
-    // blob.style.left = `${clientX}px`
-    // blob.style.top = `${clientY}px`
 
     mouseX = clientX
     mouseY = clientY
 
+    // console.log(mouseX, mouseY)
+
     blob.animate({
         left: `${mouseX}px`,
-        top: `${mouseY}px`
+        top: `${mouseY + scrollPositionY}px`
     }, {duration: 1600, fill: "forwards"})
 }
 
-// document.addEventListener('keydown', function(event){
-//     blob.animate({
-//         top: `${mouseY}px`
-//     }, {duration: 1600, fill: "forwards"})
-// })
 
+// Анимация елемента при скролле 
 document.addEventListener('scroll', function(event){
-    var scrollPosition = window.pageYOffset
-    blob.style.transform = 'translateY(' + scrollPosition + 'px)';
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    scrollPositionY = scrollPosition
+
+    // console.log(scrollPosition)
+
+    blob.animate({
+        top: `${scrollPosition+500}px`
+    }, {duration: 2000, fill: "forwards"})
+
 })
