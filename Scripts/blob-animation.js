0ee83577b0327d697e920c1b4c9c400e8d0f2e4d
Aidar <sharipovaydar200515@gmail.com>
2023-09-19 10:21:08 +0600

    Кусок цвета блоб теперь двигается вместе со скроллом. Есть баг, блоб возвращается на дефолтное свое место через некоторое время

--- Scripts/blob-animation.js
+++ Scripts/blob-animation.js
@@ -1,13 +1,31 @@
 const blob = document.getElementById("blob")
 
+var mouseX = 0
+var mouseY = 0
+
+
 document.body.onpointermove = event => {
     const { clientX, clientY} = event
 
     // blob.style.left = `${clientX}px`
     // blob.style.top = `${clientY}px`
+
+    mouseX = clientX
+    mouseY = clientY
+
     blob.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
-    }, {duration: 1800, fill: "forwards"})
+        left: `${mouseX}px`,
+        top: `${mouseY}px`
+    }, {duration: 1600, fill: "forwards"})
 }
 
+// document.addEventListener('keydown', function(event){
+//     blob.animate({
+//         top: `${mouseY}px`
+//     }, {duration: 1600, fill: "forwards"})
+// })
+
+document.addEventListener('scroll', function(event){
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    blob.style.transform = 'translateY(' + scrollPosition + 'px)';
+})
