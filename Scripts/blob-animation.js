d105f44d4bda8b7473ef4665bcde1d2919d20a13
Aidar <sharipovaydar200515@gmail.com>
2023-11-14 17:18:23 +0600

    Исправил css, wrapper с headerом накладывались друг на друга

--- Scripts/blob-animation.js
+++ Scripts/blob-animation.js
@@ -1,40 +1,40 @@
-// Референс на тот кусок цвета
-const blob = document.getElementById("blob")
+// // Референс на тот кусок цвета
+// const blob = document.getElementById("blob")
 
-// Координаты мыши
-var mouseX = 0
-var mouseY = 0
+// // Координаты мыши
+// var mouseX = 0
+// var mouseY = 0
 
-// Показывает позицию скролла по Y
-var scrollPositionY = 0
-
-
-// Анимация елемента на движения курсора, с учетом позиции скролла
-document.body.onpointermove = event => {
-    const { clientX, clientY} = event
-
-    mouseX = clientX
-    mouseY = clientY
-
-    // console.log(mouseX, mouseY)
-
-    blob.animate({
-        left: `${mouseX}px`,
-        top: `${mouseY + scrollPositionY}px`
-    }, {duration: 1600, fill: "forwards"})
-}
+// // Показывает позицию скролла по Y
+// var scrollPositionY = 0
 
 
-// Анимация елемента при скролле 
-document.addEventListener('scroll', function(event){
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+// // Анимация елемента на движения курсора, с учетом позиции скролла
+// document.body.onpointermove = event => {
+//     const { clientX, clientY} = event
 
-    scrollPositionY = scrollPosition
+//     mouseX = clientX
+//     mouseY = clientY
 
-    // console.log(scrollPosition)
+//     // console.log(mouseX, mouseY)
 
-    blob.animate({
-        top: `${scrollPosition+500}px`
-    }, {duration: 2000, fill: "forwards"})
+//     blob.animate({
+//         left: `${mouseX}px`,
+//         top: `${mouseY + scrollPositionY}px`
+//     }, {duration: 1600, fill: "forwards"})
+// }
 
-})
+
+// // Анимация елемента при скролле 
+// document.addEventListener('scroll', function(event){
+//     var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+//     scrollPositionY = scrollPosition
+
+//     // console.log(scrollPosition)
+
+//     blob.animate({
+//         top: `${scrollPosition+500}px`
+//     }, {duration: 2000, fill: "forwards"})
+
+// })
