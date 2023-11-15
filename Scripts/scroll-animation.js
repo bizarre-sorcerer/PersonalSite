b68d3b47bd095f6e3edc6f5692a123691003e64f
Aidar <sharipovaydar200515@gmail.com>
2023-11-15 20:26:01 +0600

    Начал делать анимацию при скролле. Пока хз че это будет

---
+++ Scripts/scroll-animation.js
@@ -0,0 +1,9 @@
+var welcomeText = document.getElementById("welcomeText")
+var welcomeTextPosX = welcomeText.getBoundingClientRect().top
+var scrollPositionY = 0
+
+
+window.addEventListener('scroll', function(event){
+    
+
+})
