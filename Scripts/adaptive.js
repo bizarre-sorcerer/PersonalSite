7526b7778abbf5b9d0c1479f0466727fee549918
Aidar <sharipovaydar200515@gmail.com>
2023-11-18 02:05:28 +0600

    В процессе

--- Scripts/adaptive.js
+++ Scripts/adaptive.js
@@ -1,7 +1,15 @@
-window.addEventListener("resize", function() {
-    var width = window.innerWidth;
-    var textElement = document.getElementById("My-name");
+var width = window.innerWidth;
+var textElement = document.getElementById("My-name");
 
+// По дефолту в начале проверяет размер экрана, в случае если изначально на телефону открывают
+if (width <= 600) {
+    textElement.textContent = "S. Aidar"
+} else {
+    textElement.textContent = "Sharipov Aidar" // Change the text back for screens larger than 600px
+}
+
+// Это в случае если в ручную меняют размер экрана
+window.addEventListener("resize", function() {
     // Change the text for screens 600px or less
     if (width <= 600) {
         textElement.textContent = "S. Aidar"
