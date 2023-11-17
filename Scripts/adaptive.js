768806e9bf2558d11fda801a64be27400257e8d4
Aidar <sharipovaydar200515@gmail.com>
2023-11-18 02:12:09 +0600

    Исправил ошибку. Размер экрана проверяли лишь один раз

--- Scripts/adaptive.js
+++ Scripts/adaptive.js
@@ -10,10 +10,17 @@ if (width <= 600) {
 
 // Это в случае если в ручную меняют размер экрана
 window.addEventListener("resize", function() {
+    var width = window.innerWidth;
+    var textElement = document.getElementById("My-name");    
+
     // Change the text for screens 600px or less
     if (width <= 600) {
+        console.log("a")
+        console.log(width)
         textElement.textContent = "S. Aidar"
     } else {
+        console.log(width)
+        console.log("b")
         textElement.textContent = "Sharipov Aidar" // Change the text back for screens larger than 600px
     }
 });
