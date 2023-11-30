5e553f6ca1cd9ed0b40805e2070702195b3bbe2f
Aidar <sharipovaydar200515@gmail.com>
2023-11-30 22:12:53 +0600

    asdf

--- Scripts/adaptive.js
+++ Scripts/adaptive.js
@@ -1,22 +1,10 @@
 var width = window.innerWidth;
-var textElement = document.getElementById("My-name");
-
-// По дефолту в начале проверяет размер экрана, в случае если изначально на телефону открывают
-if (width <= 600) {
-    textElement.textContent = "S. Aidar"
-} else {
-    textElement.textContent = "Sharipov Aidar" // Change the text back for screens larger than 600px
-}
 
 // Это в случае если в ручную меняют размер экрана
 window.addEventListener("resize", function() {
-    var width = window.innerWidth;
-    var textElement = document.getElementById("My-name");    
 
-    // Change the text for screens 600px or less
+    // Change the html file displayed for mobile users for mobile version of the app 
     if (width <= 600) {
-        textElement.textContent = "S. Aidar"
-    } else {
-        textElement.textContent = "Sharipov Aidar" // Change the text back for screens larger than 600px
+        window.location.href = "indexMobile.html";
     }
 });
