f32fa159237a49020373ab79059160d27d58e7df
Aidar <sharipovaydar200515@gmail.com>
2023-11-30 22:46:04 +0600

    Делаю отдельную версию сайта для мобилки

--- Scripts/adaptive.js
+++ Scripts/adaptive.js
@@ -1,4 +1,20 @@
 var width = window.innerWidth;
+var currentHtml = window.location.href 
+var indexPath = "file:///D:/Programming/WEB/PersonalSite/Html/index.html"
+var indexMobilePath = "file:///D:/Programming/WEB/PersonalSite/Html/indexMobile.html"
+
+
+// Меняет html файл в зависимости от устройства 
+if (width <= 600) {
+    if (currentHtml == indexPath){
+        window.location.href = indexMobilePath;
+    } 
+} else {
+    if (currentHtml == indexMobilePath){
+        window.location.href = indexPath;
+    }
+}
+
 
 // Это в случае если в ручную меняют размер экрана
 window.addEventListener("resize", function() {
