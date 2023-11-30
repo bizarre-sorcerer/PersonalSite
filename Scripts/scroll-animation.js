558abba3553514b1da4688e1dfe93fc2f9cc9e6a
Aidar <sharipovaydar200515@gmail.com>
2023-11-30 22:11:58 +0600

    asdf

--- Scripts/scroll-animation.js
+++
@@ -1,9 +0,0 @@
-var welcomeText = document.getElementById("welcomeText")
-var welcomeTextPosX = welcomeText.getBoundingClientRect().top
-var scrollPositionY = 0
-
-
-window.addEventListener('scroll', function(event){
-    
-
-})
