70c254d446ee89c3147e6598b10ba2a61c52b2f5
Aidar <sharipovaydar200515@gmail.com>
2024-02-14 11:30:10 +0600

    Поменял немного структуру. Частично переписнал на Vue.js

--- root_folder/src/App.vue
+++ root_folder/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+
+  <Mini_footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./assets/css/global.css"
+import Header from "./components/Header.vue"
+import Mini_footer from "./components/Mini_footer.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header, Mini_footer
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
