bfd3e765a804a3449d183b61dfa8cc1c465f552b
Aidar <sharipovaydar200515@gmail.com>
2024-02-23 13:04:35 +0600

    Добавил стек. Позже буду стилизовать его

---
+++ root_folder/src/components/Stack.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper" id="stack-wrapper">
+    <div class="main-container">
+      <div id="top-container">
+        <div id="stack-header-container">
+          <h1 id="stack-header">Стек/скиллы</h1>
+        </div>
+      </div>
+
+      <div id="bottom-container">
+        <div class="stack-container">
+          <h2>Прочий технический стек</h2>
+
+          <ul id="tech-stack">
+            <li>html, css, js, vue.js</li>
+            <li>python, flask и python модули</li>
+            <li>MySqlConnector, openCV, kivy, pyTelegramBotApi, numpy и т.д </li>
+            <li>sql</li>
+            <li>Фигма</li>
+            <li>Гуглить</li>
+          </ul>
+        </div>        
+          
+        <div class="soft-skills-container">
+          <h2>Софт скиллы</h2>
+
+          <ul id="soft-skills">
+            <li>Эмоциональный интеллект</li>
+            <li>Коммуникативность</li>
+            <li>Работа в команде</li>
+            <li>Иниицативность и са«остоятельное 
+              изучения соответствующих технологи</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>  
+</template>
+
+<style>
+@import url("../assets/css/Desktop-styles/stack.css");
+</style>
--- root_folder/src/views/MainView.vue
+++ root_folder/src/views/MainView.vue
@@ -5,6 +5,8 @@
 
   <Landing />
 
+  <Stack />
+
   <Footer />  
 </template>
 
@@ -12,13 +14,14 @@
 import "../assets/css/global.css"
 import Header from "../components/Header.vue"
 import Mini_footer from "../components/Mini_footer.vue";
-import Landing from "../components/Landing.vue";
+import Landing from "../components/Landing.vue"
 import Footer from "../components/Footer/Footer.vue"
+import Stack from "@/components/Stack.vue"
 
 export default {
   name: 'App',
   components: {
-    Header, Mini_footer, Landing, Footer
+    Header, Mini_footer, Landing, Footer, Stack
   }
 }
 
